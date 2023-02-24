cfaef12c5cd42453fd67c60b60c15abb710e40b8
kutluhann <kutluhan.sipka@ug.bilkent.edu.tr>
2023-02-24 22:56:31 +0300

    Add active category styling

--- components/categoriesArea.js
+++ components/categoriesArea.js
@@ -1,6 +1,15 @@
 import Link from "next/link"
+import { useRouter } from 'next/router'
 
-export default function categoriesAres({ categories }) {
+export default function CategoriesAres({ categories }) {
+  const router = useRouter()
+
+  function getTotalPostCount() {
+    return categories.reduce((sum, category) => {
+      return sum + category.postCount
+    }, 0)
+  }
+
   return (
     <div className="rounded-10 shadow flex justify-center p-5 flex-col w-full">
       <h3 className="title mb-4 flex items-center">
@@ -17,12 +26,15 @@ export default function categoriesAres({ categories }) {
             href="/"
             className="group h-8 cursor-pointer py-2 mx-2 text-8 flex justify-center items-center"
            >  
-            <span className="h-3 w-3 flex justify-center items-center">
-              <span 
-                className="bg-secondary w-2 h-2 rounded-sm group-hover:h-3 group-hover:w-3 transition-size"
+            <span 
+              className="h-3 w-3 flex justify-center items-center"
+            >
+              <span
+                className={'bg-secondary rounded-sm group-hover:h-3 group-hover:w-3 transition-size' 
+                  + (router.asPath == '/' ? ' w-3 h-3 ' : ' w-2 h-2 ')}
               ></span>
               </span>
-            <span className="ml-2 text-gray-600">Tüm yazılar / 2</span>
+            <span className="ml-2 text-gray-600">Tüm yazılar / {getTotalPostCount()}</span>
           </Link>
         </li>
         {categories.length && categories.map(category => (
@@ -34,7 +46,8 @@ export default function categoriesAres({ categories }) {
               <span className="h-3 w-3 flex justify-center items-center">
                 <span 
                   style={{backgroundColor: category.color}}
-                  className="w-2 h-2 rounded-sm group-hover:h-3 group-hover:w-3 transition-size"
+                  className={'rounded-sm group-hover:h-3 group-hover:w-3 transition-size' 
+                    + (router.query.id == category.id ? ' w-3 h-3 ' : ' w-2 h-2 ')}
                 ></span>
               </span>
               <span className="ml-2 text-gray-600">{category.name} / {category.postCount}</span>
