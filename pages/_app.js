8fc840744ca7686424d42fc8d8ed43e2c5b0fd5a
kutluhann <kutluhan.sipka@ug.bilkent.edu.tr>
2023-02-28 14:20:12 +0300

    Add analytics for vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/post.scss'
+import { Analytics } from '@vercel/analytics/react';
 
 import { Open_Sans } from '@next/font/google'
 const openSans = Open_Sans({ subsets: ['latin', 'latin-ext'] })
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
   return (
     <div className={openSans.className + " flex flex-col min-h-screen"}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 }
